9ad63adf4532be69473fa098115d55f87444ab61
Mason Austin <mason47austin@gmail.com>
2023-04-29 11:32:41 -0500

    made the code to allow the website to read data

--- main.js
+++ main.js
@@ -7,9 +7,38 @@ let students=[
     name:"Harry Potter",
     house: "Gryffindor"
   }
-]
+];
 
-const renderToDom= (divID, textToHtml)=>{
-  let selectedDiv=divID
-  selectedDiv.innerHTMl(textToHtml)
+const renderToDom= (divId, htmlToRender)=>{
+  const selectedDiv=document.getElementById(divId)
+  selectedDiv.innerHTML = htmlToRender
 }
+
+const cardsOnDom= () =>{
+  console.log("cardsOnDom is running");
+  let domString="";
+  students.forEach(student => {
+    domString +=`
+    <div class="${student.house}">
+    
+    </div>
+
+    <div id="studentInfo">
+      <h2>${student.name}</h2>
+      <h3>${student.house}</h3>
+      <button id="delete--${student.id}" type="button" class="btn btn-danger">Danger</button>
+    </div>`
+
+      // id:1,
+      // name:"Harry Potter",
+      // house: "Gryffindor"
+
+   });
+  renderToDom("firstYearContainer", domString)
+}
+
+const startApp= () =>{
+  cardsOnDom()
+}
+
+startApp()
