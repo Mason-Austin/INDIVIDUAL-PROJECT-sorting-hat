944473958c6ba5167bd2245eed1fccfef897122f
Mason Austin <mason47austin@gmail.com>
2023-04-29 10:28:09 -0500

    created render to dom function

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("its alive");
+
+let students=[
+  
+  {
+    id:1,
+    name:"Harry Potter",
+    house: "Gryffindor"
+  }
+]
+
+const renderToDom= (divID, textToHtml)=>{
+  let selectedDiv=divID
+  selectedDiv.innerHTMl(textToHtml)
+}
